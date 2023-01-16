dbe6003e9997744caccf4cf0f4e43f178a82eeaf
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-01-16 22:46:40 +0530

    added

--- mainpy.py
+++ mainpy.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, request
+from urllib.parse import quote_plus
+from datetime import datetime
+
 
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://lalit:Lalit@33cool@localhost/flaskdataapp'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:Lalit33#cool@localhost:3306/flaskdataapp"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = "Lalit@33cool"
 db = SQLAlchemy(app)
 
 
-class Contactsform(db.Model):
+# db.init_app(app)
+
+
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
     subject = db.Column(db.String(120), nullable=False)
-    message = db.Column(db.String(12), nullable=False)
-    date = db.Column(db.String(20), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
 
 
 @app.route('/')
@@ -41,15 +48,20 @@ def blogdetails():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if (request.method == 'POST'):
+    if request.method == 'POST':
+        print("working")
         name = request.form.get('name')
         email = request.form.get('email')
-        subject = request.form['subject']
-        message = request.form['message']
-
-        entry = Contactsform(name=name, email=email, subject=subject, message=message)
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+        print(name, email, subject, message)
+        print("working")
+        entry = Contact(name=name, email=email,
+                        subject=subject, message=message, date=datetime.now())
         db.session.add(entry)
+        print(entry)
         db.session.commit()
+        print("working2")
     return render_template('contact.html')
 
 
